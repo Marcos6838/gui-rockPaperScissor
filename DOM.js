98165db8d0734ed53653dc2b28fc37a4d2d1bd39
Marcos Hernandez <marcos510hernandez@gmail.com>
2020-06-18 22:27:37 -0700

    added hover effect for rock button still need to do paper/scissor

--- DOM.js
+++ DOM.js
@@ -4,3 +4,22 @@ rock.addEventListener('click', function (e) {
         
 });
 
+const paper = document.querySelector('#paper');
+paper.addEventListener('click',function (e){
+    console.log(e);
+});
+
+const scissor = document.querySelector('#scissor');
+scissor.addEventListener('click',function (e){
+    console.log(e);
+});
+
+let buttonHover = document.querySelector('#rock');
+buttonHover.addEventListener('mouseenter',function(e){
+    rock.classList.add('playing');
+});
+
+buttonHover.addEventListener('mouseleave',function(e){
+    rock.classList.toggle('playing');
+})
+
